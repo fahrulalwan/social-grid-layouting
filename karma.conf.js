c8f5d29143b7882ef8046ad91131a765e35201d8
Mohammad Fahrul Alwan <fahrulalwan@gmail.com>
2021-10-20 01:54:11 +0700

    rename project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/template-edo'),
+      dir: require('path').join(__dirname, './coverage/social-grid-layouting'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'template-edo'`, () => {
+  it(`should have as title 'social-grid-layouting'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('template-edo');
+    expect(app.title).toEqual('social-grid-layouting');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('template-edo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('social-grid-layouting app is running!');
   });
 });
